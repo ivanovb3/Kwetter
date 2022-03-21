d5fb69d9b41b342c9240a838113c43bc9b476cf5
ivanovb3 <rosti.rosenov@gmail.com>
2022-03-21 15:20:11 +0100

    added kubernetes, skaffold, ingress nginx

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+// const express = require('express')
+// const bodyParser = require('body-parser')
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user.js';
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use(currentUserRouter);
+
 app.listen(4000, () => {
-    console.log('Listening on port 4000!!!!!!')
+    console.log('Listening on port 4000!')
 });
 

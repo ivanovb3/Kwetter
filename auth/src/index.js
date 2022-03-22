3eb30cc8f27e7925c3704e9b99616b9effaf5a2b
ivanovb3 <rosti.rosenov@gmail.com>
2022-03-22 09:26:36 +0100

    simple validator for user creation

--- auth/src/index.js
+++ auth/src/index.js
@@ -5,11 +5,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 import { currentUserRouter } from './routes/current-user.js';
+import { signinRouter } from './routes/signin.js';
+import { signoutRouter } from './routes/signout.js';
+import { signupRouter } from './routes/signup.js';
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(4000, () => {
     console.log('Listening on port 4000!')

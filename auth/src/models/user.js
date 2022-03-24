40821c7164e47de76f8949cbd3342e83f4549e08
ivanovb3 <rosti.rosenov@gmail.com>
2022-03-24 13:46:59 +0100

    used can now be added to db with hashed password

--- auth/src/models/user.js
+++ auth/src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password.js";
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -10,6 +11,14 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
+})
 
 const User = mongoose.model('User', userSchema);
 
--- auth/src/routes/signup.js
+++ auth/src/routes/signup.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { body, validationResult } from 'express-validator'
+import { User } from '../models/user.js';
 
 const router = express.Router();
 
@@ -7,22 +8,36 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
     ],
-    (req, res) => {
+    async (req, res) => {
 
         const errors = validationResult(req);
 
+        //ToDo add error handling
         if(!errors.isEmpty()){
-            const error = new Error('Invalid email or password')
-            error.reasons = errors.array()
-            throw error;
+            // const error = new Error('Invalid email or password')
+            // error.reasons = errors.array()
+            // throw error;
+            console.log(errors)
+            return res.send({})
         }
 
         const { email, password } = req.body
+        const existingUser = await User.findOne({email});
 
-        console.log('Creating a user...');
-        
+        //ToDo add error handling
+        if (existingUser) {
+            console.log('Email in use');
+            // const error =  new Error('Email is in use')
+            // error.reasons = {msg: "Email already in use", param: 'email'}
+            // throw error
+            return res.send({});
+        }
 
-        res.send({});
+        const user = new User({email, password});
+        await user.save();
+
+        res.status(201).send(user)
+
 });
 
 export { router as signupRouter };

caf0e98b14198d6afc1bd258a3228a841d14b23e
ivanovb3 <rosti.rosenov@gmail.com>
2022-03-27 10:56:27 +0200

    auth code refactor to start automating tests

---
+++ auth/src/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import 'express-async-errors'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import { currentUserRouter } from './routes/current-user.js';
+import { signinRouter } from './routes/signin.js';
+import { signoutRouter } from './routes/signout.js';
+import { signupRouter } from './routes/signup.js';
+import { errorHandler } from './middlewares/error-handler.js';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.js
+++ auth/src/index.js
@@ -1,31 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
 
-import { currentUserRouter } from './routes/current-user.js';
-import { signinRouter } from './routes/signin.js';
-import { signoutRouter } from './routes/signout.js';
-import { signupRouter } from './routes/signup.js';
-import { errorHandler } from './middlewares/error-handler.js';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(bodyParser.json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.use(errorHandler);
+import { app } from './app.js'
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
